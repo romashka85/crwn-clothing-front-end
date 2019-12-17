338487946a3c95440f7540cac2b1b52d4b07601f
romashka85 <yulia.karaliyska@concentrix.com>
2019-12-17 22:53:58 +0200

    forms

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './form-input.style.scss';
+
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className="group">
+    <input className='form-input' onChange={handleChange} {...otherProps}/>
+  </div>
+)
